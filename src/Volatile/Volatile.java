a63599d92c0a42384545d6ee088d8852ac636fa2
Andrew-Delgado <andrewdelgado017@gmail.com>
2020-03-25 16:48:46 -0700

    added Volatile

---
+++ src/Volatile/Volatile.java
@@ -0,0 +1,41 @@
+package Volatile;
+
+// Volatile makes sure a variable gets checked by a thread and not a cached version of it.
+
+import java.util.Scanner;
+
+public class Volatile {
+
+    public static void main(String[]args){
+        Counter c = new Counter();
+        c.start();
+
+        Scanner s = new Scanner(System.in);
+        s.nextLine();
+
+        c.stopCounting();
+    }
+
+}
+
+class Counter extends Thread{
+
+    private volatile boolean counting = true;
+    private int counter = 1;
+
+    public void run(){
+        while(counting){
+            System.out.println(counter);
+            counter++;
+            try{
+                Thread.sleep(50);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stopCounting(){
+        counting = false;
+    }
+}
